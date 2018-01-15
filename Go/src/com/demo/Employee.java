373faf699b9eec230b203c9e97c3dcfbfc9497c7
sudhil <sudhil@192.168.0.24>
2018-01-14 19:38:22 -0500

    Partial implementation of displaying the numbers.

--- Go/src/com/demo/Employee.java
+++ Go/src/com/demo/Employee.java
@@ -1,13 +1,16 @@
 package com.demo;
 
 public class Employee extends Company {
+	int data[]= {0,4,42};
 
 	public Employee(String name, int employeesNumber) {
 		super(name, employeesNumber);
 		// TODO Auto-generated constructor stub
 	}
 	public void display() {
-		System.out.println("TODO:Implememt data display.");
+		for(int i= 0;i < data.length; i++) {
+			System.out.println("");
+		}
 	}
 
 }
