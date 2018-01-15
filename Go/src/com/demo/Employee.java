0b1f7365d24070c5f9809e08a1cb43a26053d555
sudhil <sudhil@192.168.0.24>
2018-01-14 19:48:57 -0500

    Completed displaying data in a SalesData object.

--- Go/src/com/demo/Employee.java
+++ Go/src/com/demo/Employee.java
@@ -1,15 +1,19 @@
 package com.demo;
 
 public class Employee extends Company {
-	int data[]= {0,4,42};
-
 	public Employee(String name, int employeesNumber) {
 		super(name, employeesNumber);
 		// TODO Auto-generated constructor stub
 	}
+
+
+	int data[]= {0,4,42};
+
+	
 	public void display() {
+		System.out.println("Data: ");
 		for(int i= 0;i < data.length; i++) {
-			System.out.println("");
+			System.out.println("Next Value: "+data[i]);
 		}
 	}
 
